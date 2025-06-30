16e0120bd151b3e8f3d77e34b727298b41915e86
willy willy <medicalawn@gmail.com>
2025-06-30 09:27:06 +0700

    initial commit

---
+++ src/main/java/willy/project250630/Project250630Application.java
@@ -0,0 +1,13 @@
+package willy.project250630;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project250630Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project250630Application.class, args);
+	}
+
+}
